e0bf7f602411475aa8a2e415ad1f34adfed533e8
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-09-04 15:14:21 +0600

    add meaningful text

--- JS/index.js
+++ JS/index.js
@@ -19,24 +19,26 @@ const displayWord = (word) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (word.length == 0) {
+    wordContainer.innerHTML = `
+     <div class="text-center col-span-full items-center rounded-xl py-10 space-y-6 font-bangla">
+       <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="font-medium text-lg text-gray-400 " >এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান।</h2>
+      </div>
+
+     `;
+  }
+
   word.forEach((word) => {
     console.log(word);
     const wordCard = document.createElement("div");
 
-/*    {
-    "id": 106,
-    "level": 2,
-    "word": "Sleep",
-    "meaning": "ঘুমানো",
-    "pronunciation": "স্লিপ"
-} */
-
-    
     wordCard.innerHTML = `
     <div class="bg-white shadow-sm rounded-xl py-10 px-5  text-center space-y-3">
-        <h1 class="font-bold text-2xl">${word.word} </h1>
+        <h1 class="font-bold text-2xl">${word.word? word.word: 'word পাওয়া যায়নি'} </h1>
         <p>Meaning /Pronounciation</p>
-        <div class="font-bangla font-medium text-2xl">"${word.meaning} / ${word.pronunciation} "</div>
+        <div class="font-bangla font-medium text-2xl">"${word.meaning ? word.meaning : 'meaning পাওয়া যায়নি' } / ${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায়নি'} "</div>
 
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
