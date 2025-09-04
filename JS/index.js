17a66e09ed1cc017b3f1de92d0d62b063496448a
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-09-04 11:45:17 +0600

    lesson added

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,30 @@
 const loadLesson = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
-    .then((json) => display(json));
+    .then((json) => display(json.data));
 };
 
 const display = (lesson) => {
   // 1.get the container & empty
-  const levelContainer = document.getElementById('level-container');
+  const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
 
   // 2. get into every data by loop
-  for(let les of lesson){
-
-  
-  
+  lesson.forEach((lesson) => {
     // 3. create a new Element
-    console.log(les);
+    console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-       <button href=""> 
-       <i class="fa-solid fa-book-open"></i>Lesson
+       <button  class="btn btn-outline btn-primary"> 
+       <i class="fa-solid fa-book-open"></i>
+       Lesson -${lesson.level_no}
        </button>
     `;
 
     // 4.append with parent and child
 
-   levelContainer.appendChild(btnDiv)
-   }
-
+    levelContainer.appendChild(btnDiv);
+  });
 };
-
 
 loadLesson();
